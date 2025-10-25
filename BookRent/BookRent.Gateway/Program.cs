90f44def0b38ab206da5516294097f35d2f396bf
Robert Zacchia <robert.zacchia@student.uibk.ac.at>
2025-10-26 00:17:15 +0200

    project setup

---
+++ BookRent/BookRent.Gateway/Program.cs
@@ -0,0 +1,48 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// --- Add Authentication & JWT configuration ---
+var jwtKey = builder.Configuration["Jwt:Key"];
+var jwtIssuer = builder.Configuration["Jwt:Issuer"];
+
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwtIssuer,
+            ValidAudience = jwtIssuer,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey!))
+        };
+    });
+
+builder.Services.AddAuthorization();
+builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+var app = builder.Build();
+
+// --- Middleware pipeline ---
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ BookRent/Services/BookRent.Catalog/Model/Book.cs
@@ -0,0 +1,10 @@
+namespace BookRent.Catalog.Model;
+
+public class Book
+{
+    public Guid Id { get; set; }
+    public required string ISBN { get; set; }
+    public required string Title { get; set; }
+    public required string Author { get; set; }
+    public string? Description { get; set; }
+}
