8cbf4d46b7d5fb5a088913b53f2cdc56aae9cccf
Robert Zacchia <robert.zacchia@student.uibk.ac.at>
2026-01-29 01:12:12 +0100

    fixed bash script

--- project/experimental_run.sh
+++ project/experimental_run.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-echo starting quantized runs kronodroid
-fedLFP --krono --data_dir ./data/kronodroid_npz/ --runs 5
+
+RUNS=5
 
 echo starting normal runs kronodroid
-fedLFP --krono --data_dir ./data/kronodroid_npz/ --runs 5 --quantize False
+fedLFP --krono --data_dir ./data/kronodroid_npz/ --runs $RUNS --no-quantize
+
+echo starting quantized runs kronodroid
+fedLFP --krono --data_dir ./data/kronodroid_npz/ --runs $RUNS
 
 
-echo starting quantized runs cifar
-fedLFP --cifar10 --data_dir ./data/ --runs 5
 
-echo starting normal runs cifar
-fedLFP --cifar10 --data_dir ./data/ --runs 5 --quantize False
+# echo starting quantized runs cifar
+# fedLFP --cifar10 --data_dir ./data/ --runs $RUNS
+
+# echo starting normal runs cifar
+# fedLFP --cifar10 --data_dir ./data/ --runs $RUNS --no-quantized
